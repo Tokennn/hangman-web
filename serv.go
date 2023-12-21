cfd4183b0d78adffce49645e7f96909846841193
coquentin <quentin.contreau@ynov.com>
2023-12-21 12:29:34 +0100

    continue css..

--- serv.go
+++ serv.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	template, err := template.ParseFiles("./index.html", "./templates/footer.html")
+	template, err := template.ParseFiles("./home.html")
 	if err != nil {
 		log.Fatal(err)
 
@@ -17,7 +17,18 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", Home)
+	http.HandleFunc("/ingame", Inside)
+
 	fs := http.FileServer(http.Dir("./static/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.ListenAndServe(":8080", nil)
+}
+
+func Inside(w http.ResponseWriter, r *http.Request) {
+	template, err := template.ParseFiles("./ingame.html")
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	template.Execute(w, nil)
 }
